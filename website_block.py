c96f79456783cff0456dc9b64db0f7ce4a10e5fa
Arati Kulkarni <aratik711@gmail.com>
2018-01-09 17:42:48 +0530

    Add files via upload

---
+++ website_block.py
@@ -0,0 +1,26 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp="website_blocker\hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["www.techcrunch.com","techcrunch.com","www.dzone.com","dzone.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,9) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,18):
+        print("Working hours")
+        with open(hosts_path,"r+") as file:
+            content=file.read()
+            for website in website_list:
+                if website not in content:
+                    file.write(redirect + "   " + website+"\n")
+    else:
+        with open(hosts_path,"r+") as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("fun hours")
+    time.sleep(5)
